73242e3c57a5e4f9fa75bbef3f98add39843c343
haamoon <amirreza_shaban@yahoo.com>
2014-09-24 22:41:56 -0400

    MOSS plagiarism detector problem resolved.Run CS3600Project1PlagiarismCheck.run to check CS3600 project 1

--- src/edu/gatech/gradeseer/plagiarismdetection/PlagiarismDetectorException.java
+++ src/edu/gatech/gradeseer/plagiarismdetection/PlagiarismDetectorException.java
@@ -1,9 +1,11 @@
 package edu.gatech.gradeseer.plagiarismdetection;
 
+/**
+ * General exception for plagiarism detection task.
+ * @author Amirreza Shaban
+ */
+
 public class PlagiarismDetectorException extends Exception {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8547957752746162529L;
 
 	public PlagiarismDetectorException(String message) {
